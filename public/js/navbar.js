5d0bc436e6ac1149057aa47fd6b891aa30013da2
Wyatt Johnson <wyatt.johnson@iconfitness.com>
2021-05-02 14:58:58 -0600

    added styles, finished nav-bar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,6 @@
+var navIsOpen = false;
 $(document).ready(navBarBackground());
-$(window).scroll(function() { navBarBackground(); });
+$(window).scroll(navBarBackground());
 
 function navBarBackground() {
     if($(window).scrollTop() <= 35) {
@@ -9,4 +10,20 @@ function navBarBackground() {
           $('.nav').addClass('nav-dark');
           $('.nav').removeClass('nav-light');
       }
+}
+function toggleMobileNav() {
+    navIsOpen = !navIsOpen;
+    if (navIsOpen) {
+        $('.nav-links').css('height', $('.nav-links').children().length * 37 + 'px');
+        // $('.nav-background').css('height', '100%');
+        $('.nav-background').addClass('nav-background-fade-in');
+        $('.nav-background').removeClass('nav-background-fade-out');
+
+
+    } else {
+        $('.nav-links').css('height', '0px');
+        // $('.nav-background').css('height', '0px');
+        $('.nav-background').removeClass('nav-background-fade-in');
+        $('.nav-background').addClass('nav-background-fade-out');
+    }
 }
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,10 +5,11 @@
 ?>
 <div class="nav nav-light">
 <a href="{{ route('index') }}">{{ Config::get('app.name') }}</a>
-<i class="material-icons mobile-menu-open">menu</i>
-  <ul>
+<i class="material-icons mobile-menu-open" onclick="toggleMobileNav()">menu</i>
+  <ul class="nav-links">
     @foreach($pages as $page)
       <li><a class="{{ Request::routeIs($page->route) ? 'active' : ''}}" href="">{{ $page->name }}</a></li>
     @endforeach
   </ul>
 </div>
+<div class="nav-background nav-background-fade-out"></div>
