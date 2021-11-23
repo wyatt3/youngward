1a911173e2397195c7148715620374d1aa08c0c8
Wyatt Johnson <wyatt.j1834@gmail.com>
2021-11-22 17:47:34 -0700

    Added several routes needed for the site

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,30 @@ Route::get('/', 'Controller@getIndex')->name('index');
 
 Route::get('organizations/{organization_id}', 'OrganizationController@getShow')->name('organization.show');
 
+Route::group(['prefix' => 'announcements'], function() {
+  Route::get('/', 'ActivityController@getIndex')->name('announcements');
+  Route::get('/past', 'ActivityController@getPast')->name('announcements.old');
+});
 
+Route::group(['prefix' => 'activities'], function() {
+  Route::get('/', 'ActivityController@getIndex')->name('activity.index');
+  Route::get('/past', 'ActivityController@getPast')->name('activity.old');
+  Route::get('/{activity_id}', 'ActivityController@getShow')->name('activity.show');
+});
+
+
+// Admin Routes
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('/', 'Controller@getAdminIndex')->name('admin.index');
 
   Route::group(['prefix' => 'organizations'], function() {
     Route::get('/', 'OrganizationController@getAdminIndex')->name('organization.admin.index');
     Route::get('/{organization_id}', 'OrganizationController@getAdminShow')->name('organization.admin.show');
+  });
+
+  Route::group(['prefix' => 'activities'], function() {
+    Route::get('/', 'ActivityController@getAdminIndex')->name('activity.admin.index');
+    Route::get('/{activity_id}', 'ActivityController@getAdminShow')->name('activity.admin.show');
   });
 });
 
