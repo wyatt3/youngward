8e3e1d605dc093f45bd17f97c490ab35c8ea2598
Wyatt Johnson <wyatt.johnson@iconfitness.com>
2021-12-22 10:16:15 -0700

    Added media model and migration, removed navpagecontroller because I decided not to use it, and adjusted the nav page seeder slightly

--- database/seeds/navPagesTableSeeder.php
+++ database/seeds/navPagesTableSeeder.php
@@ -14,27 +14,21 @@ class NavPagesTableSeeder extends Seeder
     public function run()
     {
         $page = new Page([
-            'name' => 'Home',
-            'route' => 'index',
-            'order' => '1',
-        ]);
-        $page->save();
-        $page = new Page([
             'name' => 'Ward Activities',
             'route' => 'activity.index',
-            'order' => '3',
+            'order' => '2',
         ]);
         $page->save();
         $page = new Page([
             'name' => 'Announcements',
             'route' => 'announcements',
-            'order' => '2',
+            'order' => '1',
         ]);
         $page->save();
         $page = new Page([
             'name' => 'ChurchofJesusChrist.org',
             'href' => 'https://www.churchofjesuschrist.org',
-            'order' => '4',
+            'order' => '3',
         ]);
         $page->save();
         
