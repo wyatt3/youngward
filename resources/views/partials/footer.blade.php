6799c0c8ebad6506a3d1efc1820be6f2a469c923
Wyatt Johnson <wyatt.j1834@gmail.com>
2022-01-01 18:41:34 -0700

    actually added admin link to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-<div>* This is not an official website of the Church of Jesus Christ of Latter-Day Saints *<div>
+    <a class="admin-link" href="{{ route('admin.index') }}">Admin</a><div>* This is not an official website of the Church of Jesus Christ of Latter-Day Saints *<div>
 </footer>
