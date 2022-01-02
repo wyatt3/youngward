7a2eda3660fc7282ce87c914fa58156154dd4c31
Wyatt Johnson <wyatt.j1834@gmail.com>
2022-01-01 17:00:15 -0700

    Finished initial styling of announcements page

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -9,13 +9,13 @@
 
       @foreach($announcements as $announcement)
 
-      <div class="announcement-container">
+      <div class="announcement">
         <h2 class="announcement-title">{{ $announcement->title}}</h2>
-        <p class="announcement-date"></p>
-        <p class="announcement-"></p>
+        <p class="announcement-date">Posted on {{ date_format($announcement->created_at, "F jS, Y") }}</p>
+        <p class="announcement-content">{{ $announcement->content }}</p>
 
       </div>
-
+      <div class="post-divider"></div>
       @endforeach
       
       <a class="button view-older" href="{{ route('announcements.old') }}">View Older Announcements &gt;</a>
