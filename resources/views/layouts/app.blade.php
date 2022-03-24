ca59519f4b00804480c1fe84d895a28b1dadfb3d
Wyatt Johnson <wyatt.j1834@gmail.com>
2022-03-24 08:34:43 -0600

    Updated app layout to use the app.url env variable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,6 @@
   @yield('content')
   @include('partials.footer')
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script src="/js/navbar.js"></script>
+  <script src="{{config('app.url')}}/js/navbar.js"></script>
   </body>
 <html>
